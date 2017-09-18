6a88c02262c0c360e34f3cd2861b7d25cfb4e703
spike <morndragon@126.com>
2017-09-18 12:48:02 +0800

    update

--- multi_gpu_train/show_gpu_count.py
+++ multi_gpu_train/show_gpu_count.py
@@ -9,6 +9,7 @@ from tensorflow.python.client import device_lib
 def get_available_gpus():
     """
     查看GPU的命令：nvidia-smi
+    查看被占用的情况：ps aux | grep PID
     :return: GPU个数
     """
     local_device_protos = device_lib.list_local_devices()
