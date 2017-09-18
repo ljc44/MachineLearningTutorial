b476c2a2006dc22c65feee51f014f8e2bcd1cf46
spike <morndragon@126.com>
2017-09-18 11:40:47 +0800

    update

--- multi_gpu_train/show_gpu_count.py
+++ multi_gpu_train/show_gpu_count.py
@@ -13,7 +13,8 @@ def get_available_gpus():
     """
     local_device_protos = device_lib.list_local_devices()
     print [x.name for x in local_device_protos]
-    gpu_names = [x.name for x in local_device_protos if x.device_type == 'GPU']
+    # gpu_names = [x.name for x in local_device_protos if x.device_type == 'GPU']
+    gpu_names = [x.name for x in local_device_protos]
     return gpu_names
 
 
